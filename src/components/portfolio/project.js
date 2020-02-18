d4ae92372adeed41ac7383f986f602925de92669
stevewquezada <stevewquezada@gmail.com>
2020-02-18 15:57:04 -0400

    Move each individual project into a component

---
+++ src/components/portfolio/project.js
@@ -0,0 +1,105 @@
+
+import React, { Component } from "react"
+import ExternalButton from "../../components/master/buttons/externalButton"
+import Img from "gatsby-image"
+
+import "./project.css"
+class Project extends Component {
+    render() {
+        const projectData = this.props.projectData; 
+
+        return (
+            <div className={'row'}>
+                {
+                    // ======= Diagonal Project Layout =======
+                    projectData.project_layout === "diagonal"
+                    ?   <div className={`our__approach featured__section container-fluid d-flex justify-content-center align-items-end ${projectData.project_layout} col-xl-12`} key={`0`}>
+                            <div className="page__background">
+                                <Img fluid={projectData.project_background.localFile.childImageSharp.fluid} alt={' '} tabIndex={-1}/>
+                            </div>
+                            <div className="container">
+                                <div className="row">
+                                    <div className={'col-md-12 col-lg-10 col-xl-10 text-left'}>
+                                    <Img fixed={projectData.project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
+                                    <div dangerouslySetInnerHTML={{__html: projectData.project_description}} />
+                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={projectData.project_link}></ExternalButton>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    :''
+                }
+                {
+                    // ======= Half Left Project Layout =======
+                    projectData.project_layout === "half-left"
+                    ?   <div className={'col-md-12 col-lg-6 portfolio_half right__portfolio'} key={`2`}>
+                            <div className={'portfolio__header'}>
+                                <Img fluid={projectData.project_background.localFile.childImageSharp.fluid} alt={' '} />
+                            </div>
+                            <div className={'portfolio__inner'}>
+                                <div className={'portfolio__logo'}>
+                                    <Img fixed={projectData.project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
+                                </div>
+                                <div className={'porftolio__description'}>
+                                    <div dangerouslySetInnerHTML={{__html: projectData.project_description}} />
+                                </div>
+                                <div className={'portfolio__button'}>
+                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={projectData.project_link}></ExternalButton>
+                                </div>
+                            </div>
+                        </div>
+                    :''
+                }
+                {
+                    // ======= Half Right Project Layout =======
+                    projectData.project_layout === "half-right"
+                    ?   <div className={'col-md-12 col-lg-6 portfolio_half left__portfolio'} key={`1`}>
+                            <div className={'portfolio__header'}>
+                                <Img fluid={projectData.project_background.localFile.childImageSharp.fluid} alt={' '} />
+                            </div>
+                            <div className={'portfolio__inner'}>
+                                <div className={'portfolio__logo'}>
+                                    <Img fixed={projectData.project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
+                                </div>
+                                <div className={'porftolio__description'}>
+                                    <div dangerouslySetInnerHTML={{__html: projectData.project_description}} />
+                                </div>
+                                <div className={'portfolio__button'}>
+                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={projectData.project_link}></ExternalButton>
+                                </div>
+                            </div>
+                        </div>
+                    :''
+                }
+                {
+                    // ======= Full Width Project Layout =======
+                    projectData.project_layout === "full-width"
+                    ?   <div className={'col-md-12 col-lg-12 portfolio_full '} key={`3`}>
+                            <div className="page__background">
+                                <Img fluid={projectData.project_background.localFile.childImageSharp.fluid} alt={' '} tabIndex={-1}/>
+                            </div>
+                            <div className={'col-md-12 col-lg-6 first'}>
+
+                            </div>
+                            <div className={'col-md-12 col-lg-6 second'}>
+
+                                <div className={'portfolio__inner'}>
+                                    <div className={'portfolio__logo'}>
+                                        <Img fixed={projectData.project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
+                                    </div>
+                                    <div className={'porftolio__description'}>
+                                        <div dangerouslySetInnerHTML={{__html: projectData.project_description}} />
+                                    </div>
+                                    <div className={'portfolio__button'}>
+                                        <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={projectData.project_link}></ExternalButton>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    :''
+                }                                             
+            </div>
+        )
+    }
+}
+export default Project
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,18 +2,17 @@ import React, { Component } from "react"
 import Layout from "../layouts/index"
 import { graphql } from "gatsby"
 import {Helmet} from "react-helmet";
-import { globalHistory } from "@reach/router"
 import "./css/index.css"
 import "./css/portfolio.css"
 import BruinLogo from "../images/media/bruin-letter.png"
-import ExternalButton from "../components/master/buttons/externalButton";
-import Img from "gatsby-image"
+import Project from "../components/portfolio/project"
 
 
 class MediaPage extends Component {
   render() {
     const pageData = this.props.data.allWordpressPage.edges[0].node
     const pageAcf = this.props.data.allWordpressPage.edges[0].node.acf
+
     return (
         <Layout>
             <Helmet>
@@ -37,140 +36,11 @@ class MediaPage extends Component {
                 </section>
 
                 <section className={'projects__section container-fluid'}>
-                    <div className={'row'}>
-                        <div className={`our__approach featured__section container-fluid d-flex justify-content-center align-items-end ${pageAcf.project[0].project_layout} col-xl-12`} key={`1`}>
-                            <div className="page__background">
-                                <Img fluid={pageAcf.project[0].project_background.localFile.childImageSharp.fluid} alt={' '} tabIndex={-1}/>
-                            </div>
-                            <div className="container">
-                            <div className="row">
-                                <div className={'col-md-12 col-lg-10 col-xl-10 text-left'}>
-                                <Img fixed={pageAcf.project[0].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                <div dangerouslySetInnerHTML={{__html: pageAcf.project[0].project_description}} />
-                                <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[0].project_link}></ExternalButton>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={'row'}>
-                        <div class={'col-md-12 col-lg-6 portfolio_half left__portfolio'} key={`1`}>
-                            <div className={'portfolio__header'}>
-                                <Img fluid={pageAcf.project[1].project_background.localFile.childImageSharp.fluid} alt={' '} />
-                            </div>
-                            <div className={'portfolio__inner'}>
-                                <div className={'portfolio__logo'}>
-                                    <Img fixed={pageAcf.project[1].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                </div>
-                                <div className={'porftolio__description'}>
-                                    <div dangerouslySetInnerHTML={{__html: pageAcf.project[1].project_description}} />
-                                </div>
-                                <div className={'portfolio__button'}>
-                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[1].project_link}></ExternalButton>
-                                </div>
-                            </div>
-                        </div>
-                        <div class={'col-md-12 col-lg-6 portfolio_half right__portfolio'} key={`2`}>
-                            <div className={'portfolio__header'}>
-                                <Img fluid={pageAcf.project[2].project_background.localFile.childImageSharp.fluid} alt={' '} />
-                            </div>
-                            <div className={'portfolio__inner'}>
-                                <div className={'portfolio__logo'}>
-                                    <Img fixed={pageAcf.project[2].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                </div>
-                                <div className={'porftolio__description'}>
-                                    <div dangerouslySetInnerHTML={{__html: pageAcf.project[2].project_description}} />
-                                </div>
-                                <div className={'portfolio__button'}>
-                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[2].project_link}></ExternalButton>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={'row'}>
-                        <div class={'col-md-12 col-lg-12 portfolio_full '} key={`3`}>
-                            <div className="page__background">
-                                <Img fluid={pageAcf.project[3].project_background.localFile.childImageSharp.fluid} alt={' '} tabIndex={-1}/>
-                            </div>
-                            <div class={'col-md-12 col-lg-6 first'}>
-
-                            </div>
-                            <div class={'col-md-12 col-lg-6 second'}>
-
-                                <div className={'portfolio__inner'}>
-                                    <div className={'portfolio__logo'}>
-                                        <Img fixed={pageAcf.project[3].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                    </div>
-                                    <div className={'porftolio__description'}>
-                                        <div dangerouslySetInnerHTML={{__html: pageAcf.project[3].project_description}} />
-                                    </div>
-                                    <div className={'portfolio__button'}>
-                                        <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[3].project_link}></ExternalButton>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={'row'}>
-                        <div class={'col-md-12 col-lg-6 portfolio_half left__portfolio'} key={`4`}>
-                            <div className={'portfolio__header'}>
-                                <Img fluid={pageAcf.project[4].project_background.localFile.childImageSharp.fluid} alt={' '} />
-                            </div>
-                            <div className={'portfolio__inner'}>
-                                <div className={'portfolio__logo'}>
-                                    <Img fixed={pageAcf.project[4].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                </div>
-                                <div className={'porftolio__description'}>
-                                    <div dangerouslySetInnerHTML={{__html: pageAcf.project[4].project_description}} />
-                                </div>
-                                <div className={'portfolio__button'}>
-                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[4].project_link}></ExternalButton>
-                                </div>
-                            </div>
-                        </div>
-                        <div class={'col-md-12 col-lg-6 portfolio_half right__portfolio'} key={`5`}>
-                            <div className={'portfolio__header'}>
-                                <Img fluid={pageAcf.project[5].project_background.localFile.childImageSharp.fluid} alt={' '} />
-                            </div>
-                            <div className={'portfolio__inner'}>
-                                <div className={'portfolio__logo'}>
-                                    <Img fixed={pageAcf.project[5].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                </div>
-                                <div className={'porftolio__description'}>
-                                    <div dangerouslySetInnerHTML={{__html: pageAcf.project[5].project_description}} />
-                                </div>
-                                <div className={'portfolio__button'}>
-                                    <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[5].project_link}></ExternalButton>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={'row'}>
-                        <div class={'col-md-12 col-lg-12 portfolio_full dark '} key={`6`}>
-                            <div className="page__background">
-                                <Img fluid={pageAcf.project[6].project_background.localFile.childImageSharp.fluid} alt={' '} tabIndex={-1}/>
-                            </div>
-                            <div class={'col-md-12 col-lg-6 first'}>
-
-                                <div className={'portfolio__inner'}>
-                                    <div className={'portfolio__logo'}>
-                                        <Img fixed={pageAcf.project[6].project_logo.localFile.childImageSharp.fixed} alt={'Client Logo'} tabIndex={0}/>
-                                    </div>
-                                    <div className={'porftolio__description'}>
-                                        <div dangerouslySetInnerHTML={{__html: pageAcf.project[6].project_description}} />
-                                    </div>
-                                    <div className={'portfolio__button'}>
-                                        <ExternalButton buttonClass={'md-btn'} buttonText={'Learn More'} redirectionLink={pageAcf.project[6].project_link}></ExternalButton>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class={'col-md-12 col-lg-6 second'}></div>
-                        </div>
-                    </div>
+                    {
+                        pageAcf.project.map((element, index) => (
+                            <Project projectData={element} key={index}  />
+                        ))
+                    }
                 </section>
             </div>
         </Layout>

4c5e809a6030d6c7706c70aded8e2508e3c8bce2
Wistremiro Pulido <wistremiro@raxo.tv>
2021-01-14 19:35:39 -0400

    new logo and css changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,5 @@
-import React, {Component} from "react"
-import Link from 'gatsby-link'
+import React, { Component } from "react"
+import Link from "gatsby-link"
 import { globalHistory } from "@reach/router"
 import TopMenu from "../menu/topMenu"
 import "./header.css"
@@ -7,71 +7,80 @@ import Img from "gatsby-image"
 import Logo from "../../images/logo/bruinsport.png"
 class Header extends Component {
   constructor(props) {
-    super(props);
-    this.state = { darkTheme: false };
+    super(props)
+    this.state = { darkTheme: false }
   }
   // This functions gets the current pathname to match the current page item
-  markCurrentPageItem(){
+  markCurrentPageItem() {
     let currentUrl = globalHistory.location.pathname
 
-    if(currentUrl !== '/'){
-      if ( [...currentUrl][currentUrl.length-1] === '/') {
-        currentUrl = [...currentUrl];
-        currentUrl.pop();
-        currentUrl = currentUrl.toString();
-        currentUrl = currentUrl.replace (/,/g, "");
+    if (currentUrl !== "/") {
+      if ([...currentUrl][currentUrl.length - 1] === "/") {
+        currentUrl = [...currentUrl]
+        currentUrl.pop()
+        currentUrl = currentUrl.toString()
+        currentUrl = currentUrl.replace(/,/g, "")
       }
     }
 
-    let currentPageItem = document.getElementById(currentUrl);
+    let currentPageItem = document.getElementById(currentUrl)
 
-    if(currentPageItem){
-      if(currentUrl === '/'){
-        currentPageItem.classList.add('its__home__page');
-      }else{
-        currentPageItem.classList.add('current__page');
+    if (currentPageItem) {
+      if (currentUrl === "/") {
+        currentPageItem.classList.add("its__home__page")
+      } else {
+        currentPageItem.classList.add("current__page")
       }
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     this.markCurrentPageItem()
 
     // Set the menu visibility to hidden
-    setTimeout(function(){ 
-      document.querySelectorAll('.main__menu')[0].style.visibility = "hidden";
-    }, 500);
+    setTimeout(function() {
+      document.querySelectorAll(".main__menu")[0].style.visibility = "hidden"
+    }, 500)
 
     window.onscroll = function() {
-      var header = document.getElementById("header__navbar");     
-      
+      var header = document.getElementById("header__navbar")
+
       if (window.pageYOffset > 1) {
-        header.classList.add("fixed-top");
+        header.classList.add("fixed-top")
       } else {
-        header.classList.remove("fixed-top");
+        header.classList.remove("fixed-top")
       }
-      
-    };
+    }
   }
 
   render() {
-      let wpLogo = false; 
-      if(this.props.siteLogo){
-        wpLogo = this.props.siteLogo
-      }
-      return (
-        <div className="navbar  header__wrapper" id="header__navbar">
-          <div className="brand__wrapper" id="brand__logo">
-            <Link to="/" className="brand__link">
-              {
-                wpLogo
-                ?<Img fixed={wpLogo.localFile.childImageSharp.fixed} backgroundColor={'#000000'} className={'brand__svg__logo wp__logo'} alt={"Bruin Sport Capital"} tabIndex={-1}/>
-                :<img src={Logo} className="brand__svg__logo " alt={"Bruin Sport Capital"} />
-              }
-            </Link>
-          </div>
-          <TopMenu />
+    let wpLogo = false
+    if (this.props.siteLogo) {
+      wpLogo = this.props.siteLogo
+    }
+    return (
+      <div className="navbar  header__wrapper" id="header__navbar">
+        <div className="brand__wrapper" id="brand__logo">
+          <Link to="/" className="brand__link">
+            {wpLogo ? (
+              <Img
+                fixed={wpLogo.localFile.childImageSharp.fixed}
+                backgroundColor={"#000000"}
+                className={"brand__svg__logo wp__logo"}
+                alt={"Bruin Capital"}
+                tabIndex={-1}
+              />
+            ) : (
+              <img
+                src={Logo}
+                className="brand__svg__logo "
+                alt={"Bruin Capital"}
+              />
+            )}
+          </Link>
         </div>
-      )
+        <TopMenu />
+      </div>
+    )
   }
 }
 export default Header
--- src/pages/404.js
+++ src/pages/404.js
@@ -3,20 +3,27 @@ import Layout from "../layouts/index"
 import SEO from "../components/seo"
 import InternalButton from "../components/master/buttons/internalButton"
 import "../pages/css/404.css"
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet"
 
 const NotFoundPage = () => (
   <Layout>
     <Helmet>
       <meta charSet="utf-8" />
-      <meta name="description" content={ 'We were not able to find the page you are looking for' }/>
-      <title>{ 'Bruin Sports Capital | Not Found' }</title>
+      <meta
+        name="description"
+        content={"We were not able to find the page you are looking for"}
+      />
+      <title>{"Bruin Capital | Not Found"}</title>
     </Helmet>
     <div className="not__found__page">
       <SEO title="404: Not found" />
       <h1>Ups!</h1>
       <p>We were not able to find the page you are looking for</p>
-      <InternalButton redirectionLink={'/'} buttonText={'Return to home'} buttonClass={'not__found__button'} />
+      <InternalButton
+        redirectionLink={"/"}
+        buttonText={"Return to home"}
+        buttonClass={"not__found__button"}
+      />
     </div>
   </Layout>
 )

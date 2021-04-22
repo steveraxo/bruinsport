b710b69b1c8e232d102bb7cc82470bfb076185b3
Wistremiro Pulido <wistremiro@raxo.tv>
2021-04-22 17:48:33 -0400

    ga test

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -65,16 +65,22 @@ const Layout = ({ children }) => {
         <html lang="en" />
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-19471254-0"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KB2MT6CNSS"
         />
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag(‘js’, new Date());
-  gtag(‘config’, ‘UA-19471254-0’);`,
+  gtag(‘config’, ‘G-KB2MT6CNSS’);`,
           }}
         />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if(true) { (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); } if (typeof ga === "function") { ga('create', 'UA-19471254-0', 'auto', {});`,
+          }}
+        ></script>
       </Helmet>
       <Header siteTitle={siteData.name} siteLogo={siteLogo} />
       <div className="master__wrapper">

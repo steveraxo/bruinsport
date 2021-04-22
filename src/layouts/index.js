326f7d7303cc571a7334c26f5817c6034aeb29ce
Wistremiro Pulido <wistremiro@raxo.tv>
2021-04-22 15:18:15 -0400

    testing ga tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -63,15 +63,15 @@ const Layout = ({ children }) => {
       <Helmet>
         <link rel="icon" href={siteFavicon} />
         <html lang="en" />
-        {/* <script src="https://www.googletagmanager.com/gtag/js?id=G-KB2MT6CNSS" />
+        <script src="https://www.googletagmanager.com/gtag/js?id=UA-19471254-0" />
         <script
           dangerouslySetInnerHTML={{
             __html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag(‘js’, new Date());
-  gtag(‘config’, ‘G-KB2MT6CNSS’);`,
+  gtag(‘config’, ‘UA-19471254-0’);`,
           }}
-        /> */}
+        />
       </Helmet>
       <Header siteTitle={siteData.name} siteLogo={siteLogo} />
       <div className="master__wrapper">

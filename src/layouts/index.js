cc798358bc80ae33059d8b5f496a759b564d26dc
Wistremiro Pulido <wistremiro@raxo.tv>
2021-04-25 01:15:05 -0400

    ga fix

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -63,7 +63,7 @@ const Layout = ({ children }) => {
       <Helmet>
         <link rel="icon" href={siteFavicon} />
         <html lang="en" />
-        <script
+        {/* <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-KB2MT6CNSS"
         />
@@ -74,13 +74,7 @@ const Layout = ({ children }) => {
   gtag(‘js’, new Date());
   gtag(‘config’, ‘G-KB2MT6CNSS’);`,
           }}
-        />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `if(true) { (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); } if (typeof ga === "function") { ga('create', 'UA-19471254-0', 'auto', {});`,
-          }}
-        ></script>
+        /> */}
       </Helmet>
       <Header siteTitle={siteData.name} siteLogo={siteLogo} />
       <div className="master__wrapper">

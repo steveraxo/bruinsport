854dcae2f35d598a19bce1d74019fa4518680364
Wistremiro Pulido <wistremiro@raxo.tv>
2021-04-25 01:07:47 -0400

    ga fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-19471254-0", // Google Analytics / GA
+          "GA-194712540", // Google Analytics / GA
           // "AW-CONVERSION_ID", Google Ads / Adwords / AW
           // "DC-FLOODIGHT_ID", Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
